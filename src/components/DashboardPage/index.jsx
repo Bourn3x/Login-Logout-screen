f7a7e67a84b7f1e0853b096623aefbcc634002f6
Julian Chong <julianchong10@gmail.com>
2021-06-16 13:13:05 +0800

    refactor: implemented css modules

--- src/components/DashboardPage/index.jsx
+++ src/components/DashboardPage/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.css'
-import "./style.css"
+import styles from "./style.module.css"
 
 export function DashboardPage(props) {
   const { actions } = props
@@ -11,17 +11,17 @@ export function DashboardPage(props) {
   }
 
   return (
-    <div className="dashboard-body">
-    <div className="container my-auto">
-      <div className="col-11 col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div className="card-signout">
-          <div className="card-body">
-            <h5 className="card-title text-center">Your are now signed in!</h5>
-            <button onClick={handleSignOutClick} className="btn btn-lg btn-primary btn-block text-uppercase">Logout</button>
+    <div className={`${styles["dashboard-body"]}`}>
+      <div className={`container my-auto`}>
+        <div className={`col-11 col-sm-9 col-md-7 col-lg-5 mx-auto`}>
+          <div className={`${styles["card-signout"]}`}>
+            <div className={`${styles["card-body"]}`}>
+              <h5 className={`${styles["card-title"]} text-center`}>Your are now signed in!</h5>
+              <button onClick={handleSignOutClick} className={`btn btn-lg btn-primary btn-block text-uppercase`}>Logout</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </div>
   )
 }
--- src/components/LoginPage/index.jsx
+++ src/components/LoginPage/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import 'bootstrap/dist/css/bootstrap.css'
-import "./style.css"
+import styles from "./style.module.css"
 
 export function LoginPage(props) {
   const [email, setEmail] = useState()
@@ -18,30 +18,30 @@ export function LoginPage(props) {
   }
 
   return (
-    <div className="login-body">
-    <div className="container my-auto">
-      <div className="col-10 col-sm-9 col-md-7 col-lg-5 mx-auto">
-        <div className="card-signin">
-          <div className="card-body">
-            <h5 className="card-title text-center">Sign In</h5>
-            <form className="form-signin">
+    <div className={`${styles["login-body"]}`}>
+    <div className={`container my-auto`}>
+      <div className={`col-10 col-sm-9 col-md-7 col-lg-5 mx-auto`}>
+        <div className={`${styles["card-signin"]}`}>
+          <div className={`${styles["card-body"]}`}>
+            <h5 className={`card-title text-center`}>Sign In</h5>
+            <form className={`form-signin`}>
 
-              <div className="form-label-group">
-                <input id="inputEmail" onChange={e => setEmail(e.target.value)} className="form-control" placeholder="Email address" />
+              <div className={styles["form-label-group"]}>
+                <input id="inputEmail" onChange={e => setEmail(e.target.value)} className={`form-control`} placeholder="Email address" />
                 <label htmlFor="inputEmail">Email address</label>
               </div>
 
-              <div className="form-label-group">
-                <input type="password" id="inputPassword" onChange={e => setPassword(e.target.value)} className="form-control" placeholder="Password" />
+              <div className={styles["form-label-group"]}>
+                <input type="password" id="inputPassword" onChange={e => setPassword(e.target.value)} className={`form-control`} placeholder="Password" />
                 <label htmlFor="inputPassword">Password</label>
               </div>
 
               {
                 error &&
-                <p className="error text-center">{error}</p>
+                <p className={`${styles["error"]} text-center`}>{error}</p>
               }
 
-              <button onClick={handleSignInClick} className="btn btn-lg btn-primary btn-block text-uppercase">Sign in</button>
+              <button onClick={handleSignInClick} className={`btn btn-lg btn-primary btn-block text-uppercase`}>Sign in</button>
 
             </form>
           </div>
